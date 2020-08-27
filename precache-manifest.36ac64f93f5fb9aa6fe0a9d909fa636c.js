9782433415c50642a27870a09594b1efe0b5ca77
yashmunjal <yashmu45@gmail.com>
2020-08-28 00:11:12 +0530

    Updates

---
+++ precache-manifest.36ac64f93f5fb9aa6fe0a9d909fa636c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90c9f1b80948a7cd953f4c87a84a92e",
+    "url": "/YashMunjal/results.git/index.html"
+  },
+  {
+    "revision": "d22823afc76b645ffb06",
+    "url": "/YashMunjal/results.git/static/css/main.e50bbeeb.chunk.css"
+  },
+  {
+    "revision": "1fa87e1b66606aed767e",
+    "url": "/YashMunjal/results.git/static/js/2.8a34aa43.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/YashMunjal/results.git/static/js/2.8a34aa43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22823afc76b645ffb06",
+    "url": "/YashMunjal/results.git/static/js/main.be932887.chunk.js"
+  },
+  {
+    "revision": "0eefdd7424c3c4155175",
+    "url": "/YashMunjal/results.git/static/js/runtime-main.a41fed1c.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/YashMunjal/results.git/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "d0603bf45109e7a25a49c09277bf1ccd",
+    "url": "/YashMunjal/results.git/static/media/features-split-image-01.d0603bf4.PNG"
+  },
+  {
+    "revision": "b401738dda389f86207401e245498806",
+    "url": "/YashMunjal/results.git/static/media/features-split-image-02.b401738d.png"
+  },
+  {
+    "revision": "c21bcace87c75a60a786e0c2e64b75dd",
+    "url": "/YashMunjal/results.git/static/media/features-split-image-03.c21bcace.PNG"
+  },
+  {
+    "revision": "645db2d0b0224ef86b8b398536f7983a",
+    "url": "/YashMunjal/results.git/static/media/features-split-image-04.645db2d0.png"
+  },
+  {
+    "revision": "725032a47734a430ab55fbd9d314061b",
+    "url": "/YashMunjal/results.git/static/media/features-split-image-05.725032a4.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/YashMunjal/results.git/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/YashMunjal/results.git/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "bc0dab595ea52d38e43bee383d8f244a",
+    "url": "/YashMunjal/results.git/static/media/logo.bc0dab59.svg"
+  }
+]);
